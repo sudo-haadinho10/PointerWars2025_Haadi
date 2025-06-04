7746beda50a353943c3a6a0f236d331d6e74ef52
haadi <haadimwork@gmail.com>
2025-06-04 19:54:36 +0000

    Day Test2 ll_create and ll_delete

--- linked_list_test_program.c
+++ linked_list_test_program.c
@@ -10,14 +10,15 @@
 //#  the pound sign means to expand this parameter to the literal text of the argument that was passed to the macro.
 
 #define TEST(x) printf("Running test " #x "\n"); fflush(stdout);
-#define SUBTEST(x) printf("   Executing subtest " #x "\n"); fflush(stdout); \
-		   alarm(1);
-#define FAIL(cond,msg) if (cond) {\
-	printf("   FAIL! "); \
-	printf(#msg "\n"); \
-	exit(-1);
-
+#define SUBTEST(x) printf("    Executing subtest " #x "\n"); fflush(stdout); \
+                   alarm(1);
+#define FAIL(cond, msg) if (cond) {\
+                        printf("    FAIL! "); \
+                        printf(#msg "\n"); \
+                        exit(-1);\
+                        }
 #define PASS(x) printf("PASS!\n"); alarm(0);
+
 
 bool instrumented_malloc_fail_next             = false;
 bool instrumented_malloc_last_alloc_successful = false;
@@ -58,26 +59,22 @@ void gracefully_exit_on_suspected_infinite_loop(int signal_number) {
 	exit(1);
 }	
 
-/*void check_empty_list_properties(void) {
-	TEST(check_empty_list_properties);
-	SUBTEST(linked_list_create);
-	struct linked_list *ll=linked_list_create();
+void check_empty_list_properties(void) {
+    TEST(check_empty_list_properties);
+    SUBTEST(linked_list_create);
+    struct linked_list *ll = linked_list_create();
 
+    // Sanity check that linked_list_create() works on memory allocation success
+    FAIL((instrumented_malloc_last_alloc_successful && (ll == NULL)),
+         "linked_list_create() failed when malloc() returned a valid pointer");
+    // Check invariant that head is null when empty
+    FAIL((ll->head != NULL),
+         "ll->head is non-null in empty linked list");
 
-    	// Sanity check that linked_list_create() works on memory allocation
-    	// success.
-	FAIL((instrumented_malloc_last_alloc_successful && (ll==NULL)),
-			"linked_list_create() failed when malloc() returned a valid pointer")	
-	//Check invariant that head is null when empty.
-	//
-	FAIL((ll->head!=NULL),
-			"ll->head is non-null in empty linked list");
-
-	//linked_list_delete(ll);
-	 if(linked_list_delete(ll)){
-                printf("SUCCESS\n");
-        }
-}*/
+    
+    linked_list_delete(ll);
+    PASS(check_empty_list_properties)
+}
 
 
 
@@ -95,35 +92,10 @@ int main(void) {
 		fprintf(stderr,"Failed to register instrumented free\n");
 		return 1;
 	}
-	
-	
-	struct linked_list *l1=linked_list_create();
-
-	if(l1->head==NULL){
-		printf("success\n");
-	}
-
-	// Add a node
-    struct node *new_node = instrumented_malloc(sizeof(struct node));
-    if (new_node == NULL) {
-        fprintf(stderr, "Failed to allocate new node\n");
-        linked_list_delete(l1);
-        return 1;
-    }
-    new_node->data = 5;
-    new_node->next = NULL;
-    l1->head=new_node ;
-
-    //linked_list_delete(ll);
-         if(linked_list_delete(l1)){
-                printf("SUCCESS\n");
-        }
-    
-
-  
-	
-   //     check_empty_list_properties();
-	//check_insertion_functionality();
+	check_empty_list_properties();
 	return 0;
-}
+}	
 
+
+	
+	
