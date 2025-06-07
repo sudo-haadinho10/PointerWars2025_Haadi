5477a01effae2898f59f8aaca69071d37ef055fe
haadi <haadimwork@gmail.com>
2025-06-07 15:49:08 +0000

    Moved linked_list.c, linked_list.h, linked_list_test_program.c to week1 folder

---
+++ week1/linked_list.c
@@ -0,0 +1,240 @@
+#include "linked_list.h"
+
+// Function pointers to (potentially) custom malloc() and
+// free() functions.
+//
+static void * (*malloc_fptr)(size_t size) = NULL; //NULL initially points to nothing,holds no valid memory address
+static void   (*free_fptr)(void* addr)    = NULL;
+
+//Using the standard libc's malloc and free functions
+
+
+struct linked_list * linked_list_create(void){
+	struct linked_list *l1=malloc_fptr(sizeof(struct linked_list));
+	if(l1==NULL){
+		return NULL;
+	}
+	l1->head=NULL;
+	return l1;
+}
+
+bool linked_list_delete(struct linked_list *ll){
+	if(ll==NULL){
+		return false;
+	}
+	struct node *curr=ll->head;
+	struct node *temp;
+	while(curr!=NULL){
+		temp=curr;
+		curr=curr->next;
+		free_fptr(temp);
+	}
+	ll->head=NULL;	
+	return true;
+}
+
+size_t linked_list_size(struct linked_list *ll) {
+	size_t size=0;
+	if(ll==NULL){
+		return SIZE_MAX;
+	}
+	if(ll->head==NULL){
+		return 0;
+	}
+	struct node *curr=ll->head;
+	while(curr!=NULL){
+		curr=curr->next;
+		size+=1;
+	}
+	return size;
+}
+
+
+
+bool linked_list_insert_end(struct linked_list *ll,unsigned int data) {
+	//ll==NULL 
+	
+	struct node *temp=malloc_fptr(sizeof(struct node));
+	temp->data=data;
+	temp->next=NULL;
+	
+	if(ll->head==NULL){
+		ll->head=temp;
+		return true;
+	}
+	
+	struct node *curr=ll->head;
+	while(curr->next!=NULL){
+		curr=curr->next;
+	}
+	curr->next=temp;
+	
+	return true;
+}
+
+bool linked_list_insert_front(struct linked_list *ll,unsigned int data) {
+	struct node *temp = malloc_fptr(sizeof(struct node));
+	temp->data=data;
+	if(ll->head==NULL){
+		temp->next=NULL;
+		ll->head=temp;
+		return true;
+	}
+	temp->next=ll->head;
+	ll->head=temp;
+	return true;
+
+}
+
+bool linked_list_insert(struct linked_list *ll,	size_t index,
+		unsigned int data) 
+{
+	if(ll==NULL){
+		return false;
+	}
+	if(index==0){
+                return linked_list_insert_front(ll,data);
+                
+        }
+	
+	size_t tempindex=1;
+	struct node *curr=ll->head;
+	struct node *before;
+	struct node *temp=malloc_fptr(sizeof(struct node));
+	temp->data=data;
+	temp->next=NULL;
+	
+	while(tempindex<index+1){
+
+		if(curr->next==NULL){
+			linked_list_insert_end(ll,data);
+			return true;
+		}
+		before=curr;
+		curr=curr->next;
+		tempindex+=1;
+	}
+	before->next=temp;
+	temp->next=curr;
+	return true;
+}
+
+
+size_t linked_list_find(struct linked_list *ll,unsigned int data) {
+	if(ll->head==NULL){
+		return SIZE_MAX;
+	}
+	struct iterator *iter=linked_list_create_iterator(ll,0);
+	while(iter->current_node!=NULL){
+		if(iter->data==data){
+			return iter->current_index;
+		}
+		linked_list_iterate(iter);
+	}
+	return SIZE_MAX;
+}
+
+bool linked_list_remove(struct linked_list *ll,size_t index) {
+	if(ll->head==NULL){
+		return false;
+	}
+	struct node *curr=ll->head;	
+	struct node *before=malloc_fptr(sizeof(struct node));
+	
+	 if(index==0 && curr->next==NULL){
+                free_fptr(curr);
+                ll->head=NULL;
+                return true;
+        }
+	if(index==0){
+		before=curr->next;
+		free_fptr(curr);
+		ll->head=before;
+		return true;
+	}
+	
+	while(index>0){
+		if(curr==NULL){
+			return false;
+		}
+		before=curr;
+		curr=curr->next;
+
+		index-=1;
+	}
+	before->next=curr->next;
+	free_fptr(curr);
+	return true;
+
+}
+
+struct iterator *linked_list_create_iterator(struct linked_list *ll,size_t index) {
+		if(ll==NULL){
+			return NULL;
+		}
+		/*if(index ==NULL) {
+			return NULL;
+		}*/
+		if(ll->head==NULL){
+			return NULL;
+		}
+		struct iterator *i1=malloc_fptr(sizeof(struct iterator));
+		if(i1==NULL){
+			return NULL;
+		}
+		struct node *curr=ll->head;
+		i1->ll=ll;
+		i1->current_index=index;
+		i1->data=curr->data;
+		i1->current_node=ll->head;
+		return i1;
+ }
+ 
+
+bool linked_list_delete_iterator(struct iterator *iter) {
+	if(iter==NULL){
+		return false;
+	}
+
+	//free_fptr(iter->current_index);
+	free_fptr(iter);
+	return true;
+}
+
+
+bool linked_list_iterate(struct iterator * iter) {
+	//iter->ll==NULL checked
+	//
+	
+	struct node *curr=iter->current_node;
+	curr=curr->next;
+	if(curr==NULL){
+		iter->current_node=NULL;
+		return false;
+	}
+	iter->current_index+=1;
+	iter->data=curr->data;
+	iter->current_node=curr;
+	return true;
+	
+}
+
+
+
+bool linked_list_register_malloc(void *(*malloc)(size_t)){
+	
+	if(malloc==NULL){
+		return 1;
+	}
+
+	malloc_fptr=malloc;
+	return 0; //Success
+}
+
+bool linked_list_register_free(void (*free)(void*)){
+	if(free==NULL){
+		return 1;
+	}
+	free_fptr=free;
+	return 0;
+}
---
+++ week1/linked_list_test_program.c
@@ -0,0 +1,214 @@
+#include <signal.h>
+#include <stdbool.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include "linked_list.h"
+
+//#  the pound sign means to expand this parameter to the literal text of the argument that was passed to the macro.
+
+#define TEST(x) printf("Running test " #x "\n"); fflush(stdout);
+#define SUBTEST(x) printf("    Executing subtest " #x "\n"); fflush(stdout); \
+                   alarm(1);
+#define FAIL(cond, msg) if (cond) {\
+                        printf("    FAIL! "); \
+                        printf(#msg "\n"); \
+                        exit(-1);\
+                        }
+#define PASS(x) printf("PASS! " #x "\n"); alarm(0);
+
+
+bool instrumented_malloc_fail_next             = false;
+bool instrumented_malloc_last_alloc_successful = false;
+
+void *instrumented_malloc(size_t size) {
+	if(instrumented_malloc_fail_next) {
+		instrumented_malloc_fail_next =false;
+		instrumented_malloc_last_alloc_successful=false;
+		return NULL;
+	}
+
+	void *ptr = malloc(size);
+	instrumented_malloc_last_alloc_successful=(ptr!=NULL);
+
+	return ptr;
+}
+
+void instrumented_free(void * addr){
+	if(addr==NULL){
+		return;
+	}
+	free(addr);
+}
+
+void gracefully_exit_on_suspected_infinite_loop(int signal_number) {
+	const char* err_msg = "        Likely stuck in infinite loop! Exiting.\n";
+	ssize_t retval =write(STDOUT_FILENO,err_msg,strlen(err_msg));
+	fflush(stdout);
+	
+	 // We really don't care about whether write() succeeded or failed
+    	// or whether a partial write occurred. Further, we only install
+    	// this function to one signal handler, so we can ignore that as well.
+    	// -Wall
+	
+	(void)retval;
+	(void)signal_number; 	
+	
+	exit(1);
+}	
+
+void check_empty_list_properties(void) {
+    TEST(check_empty_list_properties);
+    SUBTEST(linked_list_create);
+    struct linked_list *ll = linked_list_create();
+
+    // Sanity check that linked_list_create() works on memory allocation success
+    FAIL((instrumented_malloc_last_alloc_successful && (ll == NULL)),
+         "linked_list_create() failed when malloc() returned a valid pointer");
+    // Check invariant that head is null when empty
+    FAIL((ll->head != NULL),
+         "ll->head is non-null in empty linked list");
+    
+    linked_list_delete(ll);
+
+    // Force the memory allocator fail, ensure that NULL is returned.
+    //
+    SUBTEST(linked_list_memory_alloc_fail);
+    instrumented_malloc_fail_next=true;
+    ll=linked_list_create();
+    FAIL(ll !=NULL,
+		    "linked_list_create() returns non-null pointer on allocation failure")
+
+     // Attempt to create an iterator for index 0.
+    //
+    SUBTEST(empty_linked_list_iterator)
+    ll=linked_list_create();
+    struct iterator * iter = linked_list_create_iterator(ll,0);
+    FAIL (iter!=NULL,
+		    "linked_list_create_iterator returned an iterator for an empty linked_list")
+	    
+    //Cleanup
+    //
+    linked_list_delete_iterator(iter);
+    linked_list_delete(ll);
+
+    PASS(check_empty_list_properties)
+}
+
+void check_insertion_functionality(void) {
+	TEST(check_insertion_functionality);
+	SUBTEST(insert_end);
+
+	 // Check insertion at end with an iterator.
+    	// Inserts 1, 2, 3, 4 into the list, verifies
+    	// data.
+    	//
+	
+	struct linked_list *ll = linked_list_create();
+	size_t ll_size = SIZE_MAX;
+	FAIL(ll == NULL,
+			"Failed to create new linked_list (#1)")
+	for(size_t i=1;i<=4;i++){
+		bool status = linked_list_insert_end(ll,i);
+		FAIL(status == false,
+				"Failed to insert node into linked_list #1")
+	}
+	struct iterator * iter = linked_list_create_iterator(ll,0);
+	FAIL(iter==NULL,
+			"Failed to create new iterator for linked_lisy (#1)")
+	
+
+	SUBTEST(iterate_over_linked_list_1)
+	for(size_t i=1;i<4;i++) {
+		FAIL(iter->data!=i,
+				"Iterator does not contain correct data for linked_list (#1)")
+		FAIL(iter->current_index !=(i-1),
+			       "Iterator does not contain correct index for linked_list (#1)")
+		//Next element.
+		//
+		linked_list_iterate(iter);	
+	}
+	linked_list_delete(ll);
+	linked_list_delete_iterator(iter);
+
+	  // Check insertion at front with an iterator.
+    	// Inserts 4, 3, 2, 1 into the list, verifies data.
+    	//
+	
+	SUBTEST(insert_front)
+	ll = linked_list_create();
+	ll_size = linked_list_size(ll);
+	FAIL(ll_size!=0,
+			"linked_list (#2) size is non-zero when created")
+	FAIL(ll==NULL,
+		"Failed to create new linked_list (#2)")
+	for(size_t i=4;i!=0;i--) {
+		bool status = linked_list_insert_front(ll,i);
+		FAIL(status==false,
+				"Failed to insert node into linked_list #2")
+	}
+	ll_size=linked_list_size(ll);
+   
+	FAIL(ll_size!=4,
+			"linked_list (#2) size was not equal to 4")
+
+	SUBTEST(iterate_over_linked_list_2)
+	iter = linked_list_create_iterator(ll,0);
+	for(size_t i=1;i<=4;i++){
+		FAIL(iter->data !=i,
+				"Iterator does not contain correct data for linked_list (#2)")
+		FAIL(iter->current_index !=  (i-1),
+				"Iterator does not contain correct index for linked_list (#2)")
+
+		//Next element.
+		//
+		linked_list_iterate(iter);
+	}
+	ll_size = linked_list_size(ll);
+	FAIL(ll_size!=4,
+			"linked_list(#2) size was not equal to 4")
+	linked_list_insert(ll,0,5);
+	ll_size=linked_list_size(ll);
+	printf("%lu\n",ll_size);
+	size_t index1=linked_list_find(ll,5);
+	printf("index %lu\n",index1);
+	
+	//
+	linked_list_remove(ll,0);
+	
+	ll_size=linked_list_size(ll);
+        printf("size%lu\n",ll_size);
+	
+	//testing linked_list_remove(ll,index);
+	index1=linked_list_find(ll,5);
+        printf("index %lu\n",index1);
+	linked_list_delete(ll);
+	linked_list_delete_iterator(iter);
+
+	PASS(check_insertion_functionality)
+}
+
+int main(void) {
+	//Set up signal handler for catching infinite loops.
+	//
+	signal(SIGALRM,gracefully_exit_on_suspected_infinite_loop);
+	// Setup instrumented memory allocation/deallocation.
+	
+	if(linked_list_register_malloc(&instrumented_malloc)){
+		fprintf(stderr, "Failed to register instrumented malloc\n");	
+		return 1;
+	}
+	if(linked_list_register_free(&instrumented_free)){
+		fprintf(stderr,"Failed to register instrumented free\n");
+		return 1;
+	}
+	check_empty_list_properties();
+	check_insertion_functionality();
+	return 0;
+}	
+
+
+	
+	
